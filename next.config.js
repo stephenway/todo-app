a7aea05b49d23d2982a1f08c2c78dc02eece62d2
Stephen Way <stephenway@users.noreply.github.com>
2022-03-18 15:29:34 -0700

    Added svgr webpack plugin for inline SVGs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   async headers() {
     return [
       {
