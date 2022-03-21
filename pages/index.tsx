7d2aafa6a411e4bebe6aecb5a5588ba03c25a199
Stephen Way <stephenway@users.noreply.github.com>
2022-03-21 07:51:19 -0700

    Chunk out big components on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import AddTodoForm from "../components/AddTodoForm";
-import TodoList from "../components/TodoList";
-import Layout from "../containers/Layout";
+import dynamic from "next/dynamic";
 import { TodoListProvider } from "../containers/TodoListProvider";
+const Layout = dynamic(() => import("../containers/Layout"));
+const AddTodoForm = dynamic(() => import("../components/AddTodoForm"));
+const TodoList = dynamic(() => import("../components/TodoList"));
 
 const IndexPage = () => {
   return (
