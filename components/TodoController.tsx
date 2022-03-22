7b46ae22f985d7066156d532e42680206689f4c4
Stephen Way <stephenway@users.noreply.github.com>
2022-03-22 07:29:24 -0700

    Update styles and prep split controller

--- components/TodoController.tsx
+++ components/TodoController.tsx
@@ -1,23 +1,27 @@
 import { useTodos } from "../containers/TodoListProvider";
 import { type Todo } from "../interfaces";
+import { HTMLProps } from "../interfaces/primitive";
 import styles from "../styles/TodoController.module.css";
 import Button from "./Button";
 import TodoFilterGroup from "./TodoFilterGroup";
 
-const TodoController = () => {
+interface TodoControllerProps extends HTMLProps<HTMLLIElement> {
+  className?: string;
+  hideFilters?: boolean;
+}
+
+const TodoController = ({
+  className,
+  hideFilters,
+  ...props
+}: TodoControllerProps): JSX.Element => {
   const { todos, clearCompleted } = useTodos();
   const numActive = todos ? todos.filter((t: Todo) => !t.complete).length : 0;
 
-  const filters = [
-    { label: "All", key: "all" },
-    { label: "Active", key: "active" },
-    { label: "Completed", key: "complete" },
-  ];
-
   return (
-    <li className={styles.root}>
+    <li className={`${styles.root} ${className || ""}`} {...props}>
       <span>{numActive} items left</span>
-      <TodoFilterGroup className={styles.filterGroup} filters={filters} />
+      {!hideFilters && <TodoFilterGroup className={styles.filterGroup} />}
       <Button className={styles.completedButton} onClick={clearCompleted}>
         Clear Completed
       </Button>
--- components/TodoFilterGroup.tsx
+++ components/TodoFilterGroup.tsx
@@ -4,11 +4,17 @@ import { HTMLProps } from "../interfaces/primitive";
 import Button from "./Button";
 
 interface TodoFilterGroupProps extends HTMLProps<HTMLSpanElement> {
-  filters: Record<string, any>[];
+  filters?: Record<string, any>[];
 }
 
+const defaultFilters = [
+  { label: "All", key: "all" },
+  { label: "Active", key: "active" },
+  { label: "Completed", key: "complete" },
+];
+
 const TodoFilterGroup = ({
-  filters,
+  filters = defaultFilters,
   ...props
 }: TodoFilterGroupProps): JSX.Element => {
   const { filterTodos, setFilterTodos } = useTodos();
